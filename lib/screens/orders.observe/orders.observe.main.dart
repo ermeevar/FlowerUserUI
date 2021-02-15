267cec33f80a0760cb2c250b76ef2048277f81a1
Ермеева Руфина Камильевна <201706@mck-ktits.ru>
2021-02-16 00:45:35 +0300

    Add Orders UI-class

---
+++ lib/screens/orders.observe/orders.observe.main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class OrdersObserveMain extends StatefulWidget{
+  @override
+  OrdersObserveMainState createState() => OrdersObserveMainState();
+}
+
+class OrdersObserveMainState extends State<OrdersObserveMain> {
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return null;
+  }
+}
