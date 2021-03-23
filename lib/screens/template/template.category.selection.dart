bc1344f702e8434250f4b8c5ea6a5f78bda87589
ermeeva.r@gmail.com <ermeeva.r@gmail.com>
2021-03-23 23:27:44 +0300

    Add count of category's templates

--- lib/screens/template/template.category.selection.dart
+++ lib/screens/template/template.category.selection.dart
@@ -1,4 +1,5 @@
 import 'package:flower_user_ui/models/template.category.dart';
+import 'package:flower_user_ui/models/template.dart';
 import 'package:flower_user_ui/models/web.api.services.dart';
 import 'package:flutter/material.dart';
 
@@ -9,18 +10,36 @@ class TemplateCategorySelection extends StatefulWidget {
 
 class TemplateCategorySelectionState extends State<TemplateCategorySelection> {
   List<TemplateCategory> _templateCategories = [];
+  List<Template> _templates = [];
 
   TemplateCategorySelectionState() {
     _getTemplateCategories();
+    _getTemplates();
   }
 
-  _getTemplateCategories() {
-    WebApiServices.fetchTemplateCategory().then((response) {
+  _getTemplateCategories() async {
+    await WebApiServices.fetchTemplateCategory().then((response) {
       var templateCategoriesData = templateCategoryFromJson(response.data);
       setState(() {
         _templateCategories = templateCategoriesData;
       });
     });
+  }
+
+  _getTemplates() async {
+    await WebApiServices.fetchTemplate().then((response) {
+      var templateData = templateFromJson(response.data);
+      setState(() {
+        _templates=templateData;
+      });
+    });
+  }
+
+  int _getCountOfCategories(TemplateCategory templateCategory) {
+    return _templates
+        .where((element) => element.templateCategoryId == templateCategory.id)
+        .toList()
+        .length;
   }
 
   @override
@@ -32,12 +51,12 @@ class TemplateCategorySelectionState extends State<TemplateCategorySelection> {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Container(
-              margin: EdgeInsets.only(top: 20, left: 10),
+              margin: EdgeInsets.only(top: 20),
               child: _header(context),
             ),
             Expanded(
               child: Container(
-                padding: EdgeInsets.symmetric(horizontal: 20),
+                padding: EdgeInsets.symmetric(horizontal: 10),
                 child: _createTemplateCategoriesListView(context),
               ),
             )
@@ -68,35 +87,46 @@ class TemplateCategorySelectionState extends State<TemplateCategorySelection> {
       itemCount: _templateCategories.length,
       itemBuilder: (context, index) {
         return GestureDetector(
-          onTap: (){
-            // await Navigator.push(
-            //     context,
-            //     MaterialPageRoute(
-            //         builder: (context) =>
-            //             TemplateCategorySelection()));
-          },
-          child: Column(
+          child: Row(
             children: [
               Container(
-                margin: EdgeInsets.symmetric(vertical: 15),
-                child: Row(
-                  children: [
-                    Text(
-                      _templateCategories[index].name,
-                      style: Theme.of(context).textTheme.body1,
-                    ),
-                    Spacer(),
-                    Icon(
-                      Icons.navigate_next,
-                      color: Color.fromRGBO(130, 147, 153, 1),
-                    )
-                  ],
+                margin: EdgeInsets.only(right: 10, top: 5, bottom: 5),
+                child: CircleAvatar(
+                  backgroundColor: Color.fromRGBO(130, 147, 153, 1),
+                  radius: 25,
+                  child: Text(
+                    _getCountOfCategories(_templateCategories[index]) != null
+                        ? _getCountOfCategories(_templateCategories[index])
+                        .toString()
+                        : "0",
+                    style: Theme.of(context).textTheme.body2,
+                  ),
                 ),
               ),
-              Divider(
-                color: Color.fromRGBO(130, 147, 153, 1),
-                height: 1,
-              ),
+              Expanded(child: Column(
+                children: [
+                  Container(
+                    margin: EdgeInsets.symmetric(vertical: 15),
+                    child: Row(
+                      children: [
+                        Text(
+                          _templateCategories[index].name,
+                          style: Theme.of(context).textTheme.body1,
+                        ),
+                        Spacer(),
+                        Icon(
+                          Icons.navigate_next,
+                          color: Color.fromRGBO(130, 147, 153, 1),
+                        )
+                      ],
+                    ),
+                  ),
+                  Divider(
+                    color: Color.fromRGBO(130, 147, 153, 1),
+                    height: 1,
+                  ),
+                ],
+              ))
             ],
           ),
         );
